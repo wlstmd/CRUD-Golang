9fa0fb9edfc1d98a3a83290dab51b8a1ce2ec898
유진승 <jinseung0327@gmail.com>
2023-12-23 17:34:00 +0900

    FEAT: update

--- init/cmd/cmd.go
+++ init/cmd/cmd.go
@@ -3,19 +3,27 @@ package cmd
 import (
 	"CRUD_SERVER/config"
 	"CRUD_SERVER/network"
+	"CRUD_SERVER/repository"
+	"CRUD_SERVER/service"
 	"fmt"
 )
 
 type Cmd struct {
-	config  *config.Config
-	network *network.Network
+	config *config.Config
+
+	network    *network.Network
+	repository *repository.Repository
+	service    *service.Service
 }
 
 func NewCmd(filepath string) *Cmd {
 	c := &Cmd{
-		config:  config.NewConfig(filepath),
-		network: network.NewNetwork(),
+		config: config.NewConfig(filepath),
 	}
+
+	c.repository = repository.NewRepository()
+	c.service = service.NewService(c.repository)
+	c.network = network.NewNetwork(c.service)
 
 	fmt.Println(c.config.Server.Port)
 
